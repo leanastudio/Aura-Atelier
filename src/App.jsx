15a49af18e0c5eb6bbd7683df2d5de0813cd0951
Anna <anna.lemmerer.1@gmail.com>
2026-05-06 01:04:55 +0200

    fix: BrowserRouter basename + GitHub Pages SPA routing

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import ServicesPage from './pages/ServicesPage'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <ScrollToTop />
       <Cursor />
       <Navbar />
